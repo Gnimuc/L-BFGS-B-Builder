3045ba5705fcb4a1c1531d0e8c6bd006986a4175
Gnimuc <qiyupei@gmail.com>
2018-03-17 18:22:17 +0800

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,31 +1,13 @@
 using BinaryBuilder
 
-# These are the platforms built inside the wizard
-platforms = [
-  BinaryProvider.Linux(:i686, :glibc),
-  BinaryProvider.Linux(:x86_64, :glibc),
-  BinaryProvider.Linux(:aarch64, :glibc),
-  BinaryProvider.Linux(:armv7l, :glibc),
-  BinaryProvider.Linux(:powerpc64le, :glibc),
-  BinaryProvider.MacOS(),
-  BinaryProvider.Windows(:i686),
-  BinaryProvider.Windows(:x86_64)
-]
-
-
-# If the user passed in a platform (or a few, comma-separated) on the
-# command-line, use that instead of our default platforms
-if length(ARGS) > 0
-    platforms = platform_key.(split(ARGS[1], ","))
-end
-info("Building for $(join(triplet.(platforms), ", "))")
-
-# Collection of sources required to build L-BFGS-B
+# Collection of sources required to build LBFGSB
 sources = [
     "http://users.iems.northwestern.edu/~nocedal/Software/Lbfgsb.3.0.tar.gz" =>
     "f5b9a1c8c30ff6bcc8df9b5d5738145f4cbe4c7eadec629220e808dcf0e54720",
+
 ]
 
+# Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
 cd Lbfgsb.3.0/
@@ -96,12 +78,31 @@ patch --ignore-whitespace < Makefile.patch
 
 make
 make install
+
 """
 
-products = prefix -> [
-    LibraryProduct(prefix,"liblbfgsb")
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    BinaryProvider.Linux(:i686, :glibc),
+    BinaryProvider.Linux(:x86_64, :glibc),
+    BinaryProvider.Linux(:aarch64, :glibc),
+    BinaryProvider.Linux(:armv7l, :glibc),
+    BinaryProvider.Linux(:powerpc64le, :glibc),
+    BinaryProvider.MacOS(),
+    BinaryProvider.Windows(:i686),
+    BinaryProvider.Windows(:x86_64)
 ]
 
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "liblbfgsb", :liblbfgsb)
+]
 
-# Build the given platforms using the given sources
-hashes = autobuild(pwd(), "L-BFGS-B", platforms, sources, script, products)
+# Dependencies that must be installed before this package can be built
+dependencies = [
+
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, "LBFGSB", sources, script, platforms, products, dependencies)
